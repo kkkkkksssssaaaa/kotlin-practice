b12378c2624d2d8e8abbd6fc7351f2a20acb3515
kkkkkksssssaaaa <kofc312@naver.com>
2024-01-27 23:35:39 +0900

    feat: initialize project

---
+++ src/test/kotlin/dev/kkkkkksssssaaaa/practice/kotlin/KotlinPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package dev.kkkkkksssssaaaa.practice.kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
