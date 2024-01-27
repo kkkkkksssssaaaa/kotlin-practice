bff49f28c5565467ce0aa2f7399b1b14e10c4b37
kkkkkksssssaaaa <kofc312@naver.com>
2024-01-28 00:22:23 +0900

    refactor: remove main class declare

--- src/main/kotlin/dev/kkkkkksssssaaaa/practice/kotlin/Main.kt
+++ src/main/kotlin/dev/kkkkkksssssaaaa/practice/kotlin/Main.kt
@@ -1,8 +1,5 @@
 package dev.kkkkkksssssaaaa.practice.kotlin
 
-class Main {
-
-    fun main(args: Array<String>) {
-        println("application run")
-    }
+fun main(args: Array<String>) {
+    println("application run")
 }
