eabfbe07d412ce95ad4b0af6aefa7157f2978b7b
kkkkkksssssaaaa <kofc312@naver.com>
2024-02-06 23:20:53 +0900

    feat: complete chapter 14

---
+++ src/main/kotlin/dev/kkkkkksssssaaaa/practice/kotlin/section3/JavaVariousClass.java
@@ -0,0 +1,75 @@
+package dev.kkkkkksssssaaaa.practice.kotlin.section3;
+
+public class JavaVariousClass {
+
+    class JavaDataClass {
+
+        private final String name;
+        private final int age;
+
+        public JavaDataClass(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+
+            JavaDataClass that = (JavaDataClass) o;
+
+            if (age != that.age) return false;
+            return name.equals(that.name);
+        }
+
+        @Override
+        public int hashCode() {
+            int result = name.hashCode();
+            result = 31 * result + age;
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return "JavaDataClass{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+        }
+    }
+
+    enum JavaEnum {
+
+        KOREA("KO"),
+        AMERICA("US");
+
+        private final String code;
+
+        JavaEnum(String code) {
+            this.code = code;
+        }
+
+        public static void handle(JavaEnum javaEnum) {
+            if (javaEnum == KOREA) {
+                // do something
+            }
+
+            if (javaEnum == AMERICA) {
+                // do something
+            }
+        }
+
+        public String getCode() {
+            return code;
+        }
+    }
+}
