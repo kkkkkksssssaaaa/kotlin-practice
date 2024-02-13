4beccbc0ec010788042a9e16b264a1e335bf9fa5
kkkkkksssssaaaa <kofc312@naver.com>
2024-02-14 00:40:30 +0900

    chore: clean build script

--- library-app/build.gradle.kts
+++ library-app/build.gradle.kts
@@ -5,6 +5,9 @@ plugins {
     java
     id("org.springframework.boot") version "2.6.8"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    // 이걸 붙이면 JPA Entity 에 기본 생성자가 없는 오류가 없어진다.
+    // TODO: 정확하게 어떤걸 지원해주는지 찾아봐야겠다.
+    id("org.jetbrains.kotlin.plugin.jpa") version "1.9.22"
 }
 
 group = "com.group"
@@ -19,6 +22,7 @@ repositories {
 }
 
 dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation ("org.springframework.boot:spring-boot-starter-web")
@@ -30,7 +34,6 @@ dependencies {
 
 tasks.withType<KotlinCompile> {
     kotlinOptions {
-        freeCompilerArgs += "-Xjsr305=strict"
         jvmTarget = "11"
     }
 }
